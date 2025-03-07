9d83f070691d2836e3e1487237f3f382eedf3d25
Ashley Smith <ashesmsmith@gmail.com>
2025-03-06 17:16:38 -0700

    Week 1 Code: Part 2 completed

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,12 +15,12 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        // PLAN
-        // Multiple the 'number' by another number, starting with 1 and increasing by 1, up to 'length' times.
-        // Create an array using the 'length' for size to hold the results
-        // Loop through 'length' times, multiplying 'number' by each index (starting from 1 to 'length')
-        // Add result to the array in current index position
-        // Return the results
+        // * PLAN
+        // Multiple the 'number' by another number, starting with 1 and increasing by 1 up to 'length' times
+        // 1. Create an array using the 'length' for size to hold the results
+        // 2. Loop through 'length' times, multiplying 'number' by each index + 1 (starting from 1 to 'length')
+        // 3. Add result to the array in current index position
+        // 4. Return the results
 
         double[] results = new double[length]; // Create array the size of 'length'
 
@@ -27,7 +29,6 @@ public static class Arrays
         {
             results[i] = number * (i + 1); // 'insert multiple at current index
         }
-
 
         return results; // return array results
     }
@@ -46,5 +47,34 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // * PLAN
+        // Move 'data' to the right by 'amount'
+        // 1. Count how many elements are in the 'data' list
+        // 2. Get the remainder(%) of 'amount'/length to find the correct amount of rotations needed
+        //      -If the remainder is 0 then no rotation is needed
+        // 3. Use GetRange to create 2 new lists for 'data' before and after 'amount' remainder index
+        // 4. Use Clear to empty the 'data' list
+        // 5. Use AddRange to add the new lists into 'data' list in reverse order
+
+        var length = data.Count; // Count how many element are in 'data' list
+        amount = amount % length; // Find the remainder/actual rotations
+
+        if (amount == 0)
+        {
+            return; // No rotation happens and the list stays the same
+        }
+
+        // Get values starting at index [0] up to index [length - amount]
+        // Store values in a new list
+        List<int> part1 = data.GetRange(0, length - amount);
+
+        // Get values starting at index [length - amount] counting up to 'amount' indexes
+        // Store values in a new list
+        List<int> part2 = data.GetRange(length - amount, amount);
+
+        data.Clear(); // Clear original list
+        data.AddRange(part2); // Add the second part of original list to the beginning
+        data.AddRange(part1); // Add the first part of the original list to the end
     }
 }
